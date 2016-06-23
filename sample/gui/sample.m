e516bb844e5e13b88a87725a343fbadaf177ae28
unknown <at15@outlook.com>
2016-06-23 17:36:16 +0800

    [GUI][Sample] Add figure with a plot on it
    using fplot to draw the sin graph

---
+++ sample/gui/sample.m
@@ -0,0 +1,76 @@
+function varargout = sample(varargin)
+% SAMPLE MATLAB code for sample.fig
+%      SAMPLE, by itself, creates a new SAMPLE or raises the existing
+%      singleton*.
+%
+%      H = SAMPLE returns the handle to a new SAMPLE or the handle to
+%      the existing singleton*.
+%
+%      SAMPLE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SAMPLE.M with the given input arguments.
+%
+%      SAMPLE('Property','Value',...) creates a new SAMPLE or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before sample_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to sample_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help sample
+
+% Last Modified by GUIDE v2.5 23-Jun-2016 17:13:39
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @sample_OpeningFcn, ...
+                   'gui_OutputFcn',  @sample_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before sample is made visible.
+function sample_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to sample (see VARARGIN)
+
+% Choose default command line output for sample
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes sample wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+axes(handles.axes1);
+fplot(@(x) sin(x));
+
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = sample_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
