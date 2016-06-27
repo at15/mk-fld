e8e6db620df3fb6bf6ae540eee26cdd998b0dd5c
unknown <at15@outlook.com>
2016-06-28 01:15:10 +0800

    [Sample][JSON] use urlread to get raw json
    - webread was introduced after 2014a, it can parse JSON automatically
    - urlread was introduced after 2006a

--- sample/json/read_json_rest.m
+++ sample/json/read_json_rest.m
@@ -3,6 +3,13 @@
 
 url = 'http://localhost:8000/json';
 data = webread(url);
-% webread can parse json by default, by I think this should be a new
-% feature in 2016a
+% webread can parse json by default, it is new feaure since 2014a
 disp(data.hello);
+
+% Add jsonlab to PATH
+addpath('../../third_party/jsonlab');
+% urlread is introduced since 2006a
+data = urlread(url);
+disp(data);
+parsed_data = loadjson(data);
+disp(parsed_data.hello);
