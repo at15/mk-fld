96c799101a4c3d1e6ac195d18493edd071ede155
at15 <at15@outlook.com>
2016-08-23 17:38:20 +0800

    [Server][DB] Use tiedot instead of sqlite3
    - remove gorm
    - add test for tiedot

--- server/server/db_test.go
+++ server/server/db_test.go
@@ -1,39 +1,24 @@
 package server
 
 import (
+	"os"
 	"testing"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/HouzuoGuo/tiedot/db"
+
 	"github.com/stretchr/testify/assert"
 )
 
-type Product struct {
-	gorm.Model
-	Code  string
-	Price uint
-}
-
-func TestSQLite(t *testing.T) {
+func TestTieDot(t *testing.T) {
 	assert := assert.New(t)
-	db, err := gorm.Open("sqlite3", "test.db")
+	tmpDir := "tmp"
+	os.RemoveAll(tmpDir)
+	defer os.RemoveAll(tmpDir)
+
+	DB, err := db.OpenDB("tmp")
 	assert.Nil(err)
-	defer db.Close()
-
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
-
-	// Create
-	db.Create(&Product{Code: "L1212", Price: 1000})
-
-	// Read
-	var product Product
-	db.First(&product, 1)                   // find product with id 1
-	db.First(&product, "code = ?", "L1212") // find product with code l1212
-
-	// Update - update product's price to 2000
-	db.Model(&product).Update("Price", 2000)
-
-	// Delete - delete product
-	db.Delete(&product)
+	err = DB.Create("jobs")
+	assert.Nil(err)
+	err = DB.Close()
+	assert.Nil(err)
 }
