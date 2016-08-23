be78bb800a9f7cdd705e3851c99c9d498d3794e5
at15 <at15@outlook.com>
2016-08-23 09:32:41 +0800

    [Server][DB] Fail using gorm and go-sqlite3
    - it requires gcc to build sqlite, so

---
+++ server/server/db_test.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/stretchr/testify/assert"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func TestSQLite(t *testing.T) {
+	assert := assert.New(t)
+	db, err := gorm.Open("sqlite3", "test.db")
+	assert.Nil(err)
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                   // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	// Update - update product's price to 2000
+	db.Model(&product).Update("Price", 2000)
+
+	// Delete - delete product
+	db.Delete(&product)
+}
