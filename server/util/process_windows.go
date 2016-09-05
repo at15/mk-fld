4d509b21bf10c6c77dec0ba2b22141e0cfcb1e48
at15 <at15@outlook.com>
2016-09-05 11:22:22 -0700

    [Server] Add mem and cpu info for process

--- server/util/process_windows.go
+++ server/util/process_windows.go
@@ -3,15 +3,17 @@
 package util
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os/exec"
 	"strings"
 )
 
 type ProcessInfo struct {
-	PID    int
-	Memory int
-	// FIXME: should parse the string and use number instead
+	PID int
+	// FIXME: should pare string
+	Memory string
+	// FIXME: should parse string and use number instead
 	CPUTime string
 	Running bool
 	Status  string
@@ -19,39 +21,41 @@ type ProcessInfo struct {
 
 func GetProcessInfo(pid int) (ProcessInfo, error) {
 	p := ProcessInfo{PID: pid}
-	// use tasklist
+	// Use tasklist to get process information
 	//  TASKLIST /FI "USERNAME ne NT AUTHORITY\SYSTEM" /FI "STATUS eq running"
-	// command := exec.Command("tasklist", "/FI", fmt.Sprintf("PID eq %d", pid), "/FO", "CSV", "/V", "/NH")
-	command := exec.Command("tasklist", "/FI", fmt.Sprintf("PID eq %d", pid), "/FO", "TABLE", "/V", "/NH")
-
 	// "映像名称","PID","会话名      ","会话#   ","内存使用 ","状态  ","用户名   ","CPU 时间","窗口标题    "
 	// "MATLAB.exe","1272","Console","3","172,564 K","Running","DESKTOP-GAC9MH5\tmp","0:00:40","MATLAB R2016a"
-	// Redirect to console
-	// command.Stdout = os.Stdout
-	// command.Stderr = os.Stderr
+	command := exec.Command("tasklist", "/FI", fmt.Sprintf("PID eq %d", pid), "/FO", "CSV", "/V", "/NH")
+
 	// Get output in bytes form
 	output, err := command.CombinedOutput()
 	if err != nil {
 		log.Warn(err)
 		return p, err
 	}
-	log.Info(string(output))
-	// cols := strings.Split(string(output), ",")
-	cols := strings.Split(string(output), " ")
-	data := make([]string, len(cols))
-	for i, v := range cols {
-		// FIXME: the last elements's " is not trimed
-		// data[i] = strings.Trim(v, "\"")
-		data[i] = strings.Trim(v, " ")
-
+	// log.Debug(string(output))
+	r := csv.NewReader(strings.NewReader(string(output)))
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Error(err)
+		return p, err
 	}
-	log.Info(cols)
-	log.Info(data)
-	// if data[5] == "Running" {
-	// 	p.Running = true
-	// } else {
-	// 	p.Running = false
+	log.Debug(records)
+	record := records[0]
+	log.Debug(record)
+	// log.Info(len(record))
+	// for _, v := range record {
+	// 	log.Info(v)
 	// }
-	// TODO: mem and cpu time
+	p.Status = record[5]
+	if record[5] == "Running" {
+		p.Running = true
+	} else {
+		p.Running = false
+	}
+	log.Info(p.Running)
+	p.Memory = record[4]
+	p.CPUTime = record[7]
+	log.Info(p)
 	return p, nil
 }
